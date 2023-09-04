b5ca36112dae3489dd7f2b8284bb4d8e36725423
Sara Sofia Mora Trujillo <134168046+chofi-chan@users.noreply.github.com>
2023-09-04 15:39:36 -0500

    PARTE 1

---
+++ main.py
@@ -0,0 +1,5 @@
+# Pedir el nombre del jugador por teclado
+nombre_jugador = input("Por favor, ingresa tu nombre: ")
+
+# Mostrar un mensaje de bienvenida con el nombre del jugador
+print("¡Bienvenido, " + nombre_jugador + "! Este es el Proyecto Integrador parte 1.")
